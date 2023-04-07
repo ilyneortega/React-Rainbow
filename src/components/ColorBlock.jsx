07691e8814ba7282393d187a51bf9e08c44227e2
Ilyne Ortega <ilyneortega@ilynes-MBP.attlocal.net>
2023-04-06 21:32:02 -0700

    first commit

---
+++ src/components/ColorBlock.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function ColorBlock({ color }) {
+    let style = { 'backgroundColor': color }
+    return <div className="colorBlock" style={style}>
+        <p>{color}</p>
+    </div>
+}
+
+export default ColorBlock;
